8ee0a9cfbac1a088d09fa05776792623aab03d0b
SAURABH JAIN <jainsaurabh1995@gmail.com>
2023-03-19 02:25:44 +0530

    Create Day Logic.py

---
+++ Day Logic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import calendar
+from datetime import timedelta, date
+import datetime
+import os, sys
+from os import name
+from dateutil import relativedelta
+
+tabel1 = pd.read_sql("Day logic dummy.csv")
